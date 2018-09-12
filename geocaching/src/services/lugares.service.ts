4c2ba739faa933ddad2cfc0ce64a1abc2cd09df6
Cristian Franco <cristianfranco@Cristians-MacBook-Pro.local>
2018-09-12 14:37:41 -0500

    Agregar servicio de acceso a datos
    get, create, edit, delete

---
+++ geocaching/src/services/lugares.service.ts
@@ -0,0 +1,25 @@
+import { AngularFireDatabase } from "angularfire2/database";
+import { Injectable } from "@angular/core";
+
+// This is injectable service to another component
+@Injectable()
+export class LugaresService {
+    constructor(public afDB: AngularFireDatabase) {
+
+    }
+    public getLugares() {
+        return this.afDB.list('/lugares/');
+    }
+    public getLugar(id) {
+        return this.afDB.object('/lugares/' + id);
+    }
+    public createLugare(lugar) {
+        return this.afDB.database.ref('/lugares/' + lugar.id).set(lugar);
+    }
+    public editLugare(lugar) {
+        return this.afDB.database.ref('/lugares/' + lugar.id).set(lugar);
+    }
+    public deletePlace(lugar){
+        return this.afDB.database.ref('/lugares/' + lugar.id).remove();
+    }
+}
